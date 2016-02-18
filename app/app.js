752576218a29efb57b07bb016858deccaaa700f3
Allan Rojas <allanjose91@gmail.com>
2016-02-18 15:01:02 -0600

    Index

---
+++ app/app.js
@@ -0,0 +1,3 @@
+/**
+ * Created by Allan RD on 18/02/2016.
+ */
---
+++ app/main.ts
@@ -0,0 +1,7 @@
+/**
+ * Created by Allan RD on 18/02/2016.
+ */
+import {bootstrap}    from 'angular2/platform/browser'
+import {AppComponent} from './app.component'
+
+bootstrap(AppComponent);
